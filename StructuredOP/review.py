50f099a1d601067f793c2d6c9555b2b67e7eab49
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-06-06 23:39:30 +0530

    Review systen with structured output

---
+++ StructuredOP/review.py
@@ -0,0 +1,46 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+
+#schema for the data-format of the final result that will be generated
+
+class Review(TypedDict):
+    summary: str
+    sentiment: str
+    pros: str
+    cons: str
+    reviewer: str
+
+structuredModel = model.with_structured_output(Review)
+
+result = structuredModel.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Priyanshu Paul
+""")
+
+
+print("Structured Output: ", result)
+print()
+print("Summary: ", result['summary'])
+print()
+print("Sentiment: ", result['sentiment'])
+print()
+print("Pros: ", result['pros'])
+print()
+print("Cons: ", result['cons'])
+print()
+print("Review By: ", result['reviewer'])
